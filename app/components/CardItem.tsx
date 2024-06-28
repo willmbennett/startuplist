53ecd58b40e3b61592ce63d8975c82d79b5af847
willmbennett <wbennett711@gmail.com>
2024-06-28 16:44:56 -0700

    fixed image use

--- app/components/CardItem.tsx
+++ app/components/CardItem.tsx
@@ -10,6 +10,7 @@ import {
 import { StartupType } from "../types";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 export function CardItem({ startup }: { startup: StartupType }) {
     const {
@@ -28,11 +29,13 @@ export function CardItem({ startup }: { startup: StartupType }) {
     return (
         <Card className="w-full p-4 shadow-md hover:shadow-lg transition-shadow duration-300">
             <CardHeader>
-                <div className="flex justify-center mb-4">
-                    <img src={image} alt={`${name} logo`} className="w-32 h-32 object-cover rounded-full border border-gray-200 shadow-sm" />
-                </div>
-                <div className="flex gap-2">
-                    <CardTitle className="text-xl font-semibold">{name}</CardTitle>
+                <div className="flex items-center justify-between">
+                    <div className="flex gap-2 justify-start items-center">
+                        <div className="flex justify-center w-12 h-12 rounded-full border border-gray-200 shadow-sm overflow-hidden relative">
+                            <Image src={image} alt={`${name} logo`} fill objectFit="cover" />
+                        </div>
+                        <CardTitle className="text-xl font-semibold">{name}</CardTitle>
+                    </div>
                     <div className="py-1 px-2 backdrop-blur-md bg-white/10 rounded-lg">
                         <p>{founded}</p>
                     </div>
@@ -43,10 +46,10 @@ export function CardItem({ startup }: { startup: StartupType }) {
                 <div className="grid w-full items-center gap-4">
                     <div className="flex flex-col space-y-2">
                         <Link href={website} className="text-blue-100 hover:underline">Website</Link>
-                        <p><strong>Industries:</strong></p>
+                        <p><strong>Industries</strong></p>
                         <div className="flex flex-wrap gap-2">
                             {industries.map(i =>
-                                <div className="py-1 px-2 backdrop-blur-md bg-white/10 rounded-lg">
+                                <div key={i} className="py-1 px-2 backdrop-blur-md bg-white/10 rounded-lg text-sm">
                                     <p>{i}</p>
                                 </div>
                             )}
@@ -58,10 +61,10 @@ export function CardItem({ startup }: { startup: StartupType }) {
             </CardContent>
             <CardFooter className="flex justify-center gap-2">
                 <div className="py-1 px-2 backdrop-blur-md bg-white/10 rounded-lg">
-                    <p>YC {yc_batch}</p>
+                    <p className="text-sm">YC {yc_batch}</p>
                 </div>
                 <div className={cn("py-1 px-2 rounded-lg bg-opacity-50", status == "Active" ? 'bg-green-300' : 'bg-red-300')}>
-                    <p>{status}</p>
+                    <p className="text-sm">{status}</p>
                 </div>
             </CardFooter>
         </Card>
--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'bookface-images.s3.amazonaws.com',
+        port: '',
+        pathname: '/logos/**',
+      },
+    ],
+  },
   rewrites: async () => {
     return [
       {
